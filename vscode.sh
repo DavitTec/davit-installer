85c276753a5df26fa21bcea8a4102d98c821c471
David Mullins <me@davit.ie>
2025-08-28 23:53:27 +0200

    fix: vscode stub header/footer

--- vscode.sh
+++ vscode.sh
@@ -1,3 +1,10 @@
-#!/bin/bash
-# Stub to call central vscode.sh
-/opt/davit/bin/vscode.sh -p "$(pwd)" "$@"
+#!/usr/bin/env bash
+# vscode.sh
+# Version: 0.0.1
+# Description: Stub to call central vscode.sh for project setup.
+# Alias: Generic
+
+# Call central vscode.sh (no duplicates; assumes central handles env/manifest)
+ /opt/davit/bin/vscode.sh -p "$(pwd)" "$@"
+
+# End of vscode.sh
